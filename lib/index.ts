7011488b8a0394c1774fde326dd438c6ab645c7a
gboysking <gboyskingq@gmail.com>
2023-04-14 00:04:31 +0000

    Refactor onReady function to handle error in onReadyPromises
    Commit message: Refactor onReady function to handle error in onReadyPromises

--- lib/index.ts
+++ lib/index.ts
@@ -98,7 +98,13 @@ export class TimeSeriesStatisticsManager {
             } else if (this.state === 'FAIL') {
                 reject();
             } else {
-                this.onReadyPromises.push(resolve);
+                this.onReadyPromises.push((error) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve();
+                    }
+                });                
             }
         });
     }
